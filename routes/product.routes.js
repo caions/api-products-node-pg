b2165894e883e4deda9bdcd397f18b092f0d5b5f
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2021-12-16 23:12:49 -0300

    feat: middleware verifyJwt

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const route = express.Router();
-const productController = require('../controllers/product.controllers')
+const productController = require("../controllers/product.controllers");
+const verifyJWT = require("../controllers/verifyJwt");
 
+route.post("/auth", productController.auth);
+
+route.use(verifyJWT);
 route.get("/", productController.index);
-route.get("/:id", productController.show);
+route.get("/:id", verifyJWT, productController.show);
 route.post("/", productController.create);
 route.put("/:id", productController.update);
 route.delete("/:id", productController.destroy);
