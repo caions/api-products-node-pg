eb7a7ef2ecc9dc13eb14b866c9b81b709d28f9b5
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2021-12-29 22:39:39 -0300

    fix: handler error incorrect path

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const app = express();
 const routes = require("./routes/index");
-const handlerError = require("./utils/apiError");
+const handlerError = require("./utils/handleError");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
