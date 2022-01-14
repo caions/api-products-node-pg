f25bf7856e4e5127277309b065d986dc960e03c1
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2022-01-14 14:51:06 -0300

    feat: DeleteProductService new Errors

--- app/services/Product/DeleteProductService.js
+++ app/services/Product/DeleteProductService.js
@@ -6,6 +6,14 @@ class DeleteProductService {
   }
 
   async execute(id) {
+    if (!id) {
+      throw new ApiError(404, "O Id do Produto não foi informado");
+    }
+
+    if (isNaN(id)) {
+      throw new ApiError(404, "O Id do Produto deve do tipo number");
+    }
+
     let checkProductExists = await this.productRepository.findById(id);
     if (!checkProductExists) {
       throw new ApiError(404, "Produto não encontrado");
